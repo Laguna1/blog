f5757ae25e0f7966016b6295e7749076bdd5af84
Laguna1 <lagunazoomarket@gmail.com>
2021-01-17 01:08:42 +0200

    Add Using Concerns with correct validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_222039) do
+ActiveRecord::Schema.define(version: 2021_01_16_224728) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
